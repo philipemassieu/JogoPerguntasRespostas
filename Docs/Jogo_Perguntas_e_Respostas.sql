fc9552b65e5009512a4139109ad6675832c0612f
philipe massieu <philipemassieupm@gmail.com>
2017-02-01 17:04:18 -0200

    atualizaço do banco de dados

--- Docs/Jogo_Perguntas_e_Respostas.sql
+++ Docs/Jogo_Perguntas_e_Respostas.sql
@@ -11,3 +11,29 @@ select * from tb_Jogador
 
 insert into tb_Jogador(nome) values('Amanda')
 insert into tb_Jogador(nome) values('Fernanda')
+
+create table tb_Perguntas(
+   id int identity primary key,
+   pergunta	varchar(max),
+   resposta_correta varchar(max),
+   data	datetime default getdate()
+   )
+   
+   select * from tb_Perguntas
+   
+   insert into tb_Perguntas(pergunta, resposta_correta)
+   values('Quanto é 2 + 2?','4')
+   
+   
+    
+   
+   select * from tb_Jogador
+   
+   delete from tb_jogador where id = 13
+   
+   insert into tb_Jogador(nome) values('ramón')
+   
+   delete from tb_jogador
+   
+   --limpa os dados da tabela e reinicia o campo identity
+   truncate table tb_jogador
